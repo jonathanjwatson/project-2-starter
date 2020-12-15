d93b1e830a77834d63be12677b5a760fba57fc36
Ayanna Aziz <ayanna.aziz77@gmail.com>
2020-12-15 13:37:22 -0500

    m

--- models/player.js
+++ models/player.js
@@ -1,10 +1,11 @@
 module.exports = function (sequelize, DataTypes) {
   const Player = sequelize.define("Player", {
     email: DataTypes.STRING,
-    password: DataTypes.STRING,
+    numGuest: DataTypes.INTEGER,
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     phone: DataTypes.STRING,
+    reason: DataTypes.STRING,
     // TODO: Days of the week?
   });
 
