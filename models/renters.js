a01c9831ad34adb5e82afa6c985d973f0484e545
Somer Jackson <somer.jackson@gmail.com>
2020-12-17 11:07:33 -0500

    altered renters form

--- models/renters.js
+++ models/renters.js
@@ -1,23 +1,22 @@
 module.exports = function (sequelize, DataTypes) {
-    const Renters = sequelize.define("Renters", {
-      email: DataTypes.STRING,
-      numGuest: DataTypes.INTEGER,
-      firstName: DataTypes.STRING,
-      lastName: DataTypes.STRING,
-      phone: DataTypes.STRING,
-      reason: DataTypes.STRING,
-      propertyID: DataTypes.STRING,
-      bookingID: DataTypes.STRING,
-      // TODO: Days of the week?
+  const Renters = sequelize.define("Renters", {
+    email: DataTypes.STRING,
+    numGuest: DataTypes.INTEGER,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    reason: DataTypes.STRING,
+    propertyID: DataTypes.STRING,
+    bookingID: DataTypes.STRING,
+    // TODO: Days of the week?
+  });
+
+  Renters.associate = function (models) {
+    Renters.belongsToMany(models.Host, {
+      through: "Bookings",
+      foreignKey: "bookingID",
     });
-  
-    Renters.associate = function (models) {
-      Renters.belongsToMany(models.Host, {
-        through: "Bookings",
-        foreignKey: "bookingID",
-      });
-    };
-  
-    return Renters;
   };
-  
+
+  return Renters;
+};
