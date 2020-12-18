e9bb5793a17001cb81aaffb804740ae0c7e371d4
Phlipper <marzullop73@gmail.com>
2020-12-18 08:06:15 -0500

    moved css file into new css folder in public/assets folders, defined host model in host.js file

---
+++ models/host.js
@@ -0,0 +1,15 @@
+const { STRING } = require("sequelize/types");
+
+module.exports = function (sequelize, DataTypes) {
+    const Host = sequelize.define("Host", {
+      email: DataTypes.STRING,
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      phone: DataTypes.STRING,
+      propertyTypeID: DataTypes.STRING,
+      propertyDescribe: DataTypes.STRING,
+    //   bookingID: DataTypes.STRING
+    
+    });
+
+    return Host};
